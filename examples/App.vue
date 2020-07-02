e6fa1350ba2fd8097584396996053e5867c10b54
EDZ <wangzijain@mindflow.com.cn>
2020-07-02 11:03:14 +0800

    vprincetab

---
+++ examples/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <vPrinceTab />
+  </div>
+</template>
+
+<script>
+import vPrinceTab from './components/vPrinceTab.vue'
+
+export default {
+  name: 'App',
+  components: {
+    vPrinceTab
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-color: rgb(244,244,244);
+  padding-bottom: 60px;
+}
+</style>
