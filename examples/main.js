17701eea5556bf4ad52d028d95ccd85fa9afdf74
EDZ <wangzijain@mindflow.com.cn>
2020-07-02 14:08:48 +0800

    bug

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/common.css';
+import vPrinceTab from "../packages/index";
+Vue.use(vPrinceTab)
 Vue.config.productionTip = false;
 
 new Vue({
